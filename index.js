712b758a460a8ba9d300c1195b005b2cd1dd9a56
juansmolano <juansmolano@gmail.com>
2018-04-17 15:00:22 -0500

    Clean unused entites and fix index.js exports

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load();
 }
 
-const EventStore = require('./lib/EventStore');
-const Event = require('./lib/entities/Event');
-const RetrieveEventResult = require('./lib/entities/RetrieveEventResult');
-const RetrieveNewAggregateResult = require('./lib/entities/RetrieveNewAggregateResult');
+const EventStore = require('./lib/EventStore').EventStore;
+const Event = require('./lib/entities/Event').Event;
 
 
 module.exports = {
     EventStore,
     Event,
-    RetrieveEventResult,
-    RetrieveNewAggregateResult
 };
